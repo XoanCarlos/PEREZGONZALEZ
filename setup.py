b8545c2d391293742c33bb26ba31c7e9f5b32da4
JuanCarlos <fVf996p1>
2021-01-31 21:52:53 +0100

    v0.0.1 Mejoras menores

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='PEREZGONZALEZ',
+    version='0.0.1',
+    packages=[''],
+    url='http://www.loquesea.com',
+    license='',
+    author='JuanCarlos',
+    author_email='correo@gmail.com',
+    description='Distrib. Programa'
+)
